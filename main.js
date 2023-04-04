aa4e090bac08be078076640964eea9c75cb914ac
Omid Alikhtl <omidrazi_1989@yahoo.com>
2023-04-04 11:51:16 -0700

    query selectors added

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //  IIFE are those type of function htat automatically runs when project is run
 // IIFE = imediatly invoked function expressions.
 // iife = has different types.
-(function () {});
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+});

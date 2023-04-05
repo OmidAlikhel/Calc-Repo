07f52bfac14da1929dd73405235f0c13735b3410
Omid Alikhtl <omidrazi_1989@yahoo.com>
2023-04-04 18:48:10 -0700

    equal sign and numbers coded added

--- main.js
+++ main.js
@@ -6,4 +6,25 @@
   let buttons = document.querySelectorAll(".btn");
   let clear = document.querySelector(".btn-clear");
   let equal = document.querySelector(".btn-equal");
-});
+  console.log(buttons);
+
+  //  first functionality
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let userInput = e.target.dataset.num;
+      screen.value += userInput;
+    });
+  });
+  // second functionality : equal sign
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+})();
+
+console.log("hello word");
